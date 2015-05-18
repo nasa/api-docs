8e12972a82943866f6e30e0756c3d01bc6718019
Jason Duley <jason.duley@nasa.gov>
2015-05-18 09:21:55 -0700

    last edits before deploy

--- javascripts/menu-notoc.js
+++ javascripts/menu-notoc.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
 	$("input[value='home']").remove();
 	$("#home").remove();
 	$("#tocify-header0 .tocify-item a").on("click", function() {
-		window.location.href = "/landing.html";
+		window.location.href = "/";
 	});
 	
 	$("input[value='api-listing']").remove();
 	$("#api-listing").remove();
 	$("#tocify-header1 .tocify-item a").on("click", function() {
-		window.location.href = "/#authentication";
+		window.location.href = "/api.html#authentication";
 	});
 	
 	$("input[value='getting-started-link']").remove();
 	$("#getting-started-link").remove();
 	$("#tocify-header1 .tocify-item a").on("click", function() {
-		window.location.href = "/landing.html#getting-started";
+		window.location.href = "/#getting-started";
 	});
 });
