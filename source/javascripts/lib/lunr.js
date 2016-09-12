0a56d5bca5e5e6f68625e5707e441b7ef161875b
Feram <feram@feram.co>
2016-09-12 20:29:01 +0000

    Fix typo enviroments

--- source/javascripts/lib/lunr.js
+++ source/javascripts/lib/lunr.js
@@ -1861,7 +1861,7 @@ lunr.TokenStore.prototype.toJSON = function () {
     } else if (typeof exports === 'object') {
       /**
        * Node. Does not work with strict CommonJS, but
-       * only CommonJS-like enviroments that support module.exports,
+       * only CommonJS-like environments that support module.exports,
        * like Node.
        */
       module.exports = factory()
